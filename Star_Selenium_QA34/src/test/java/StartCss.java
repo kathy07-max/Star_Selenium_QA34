41ec0418d9d48a54d1512a81ac48e14f730d5c2d
kathy07-max <ksheva0209@gmail.com>
2022-06-15 23:17:50 +0300

    h_workCss

--- Star_Selenium_QA34/src/test/java/StartCss.java
+++ Star_Selenium_QA34/src/test/java/StartCss.java
@@ -16,5 +16,29 @@ public class StartCss {
         WebElement el1 =wd.findElement(By.id("root"));
         WebElement el2  =wd.findElement(By.tagName("a"));
 
+
+    }
+
+    @Test
+    public void h_workCss(){
+        wd=new ChromeDriver();
+        wd.navigate().to("https://contacts-app.tobbymarshall815.vercel.app/home");
+
+        WebElement loginTab=wd.findElement(By.cssSelector("[href='/login']"));
+        loginTab.click();
+
+        WebElement emailTextBox=wd.findElement(By.cssSelector("[placeholder='Email']"));
+        emailTextBox.click();
+        emailTextBox.clear();
+        emailTextBox.sendKeys("shevchenk@mail.ru");
+
+        WebElement passwordTextBox=wd.findElement(By.cssSelector("[placeholder='Password']"));
+        passwordTextBox.click();
+        passwordTextBox.clear();
+        passwordTextBox.sendKeys("020985$Max");
+
+        WebElement registrationButton=wd.findElement(By.cssSelector("div.login_login__3EHKB :last-child"));
+        registrationButton.click();
+
     }
 }
